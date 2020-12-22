26882951468e61a0339123dcb3ed19d2f75b8744
compo-js <73560943+compo-js@users.noreply.github.com>
2020-12-22 15:05:31 +0300

    new

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const postcss = require('postcss')
 
 module.exports = function(opts = {}) {
   return through2(function(file, encoding, callback) {
-    file.contents = Buffer.from(file.contents.toString().replace(/(?<=<style>)([^]+?)(?=<\/style>)/g,
+    file.contents = Buffer.from(file.contents.toString().replace(/(?<=<style[^>]*?>)([^]+?)(?=<\/style>)/g,
       (match, fixing) => postcss([autoprefixer(opts)]).process(fixing).css)
     )
     callback(null, file)
